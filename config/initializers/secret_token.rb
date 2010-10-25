2bdd6691eda150c9fa48a7fa9e281ac5aa351905
Greg Sterndale <gsterndale@gmail.com>
2010-10-24 20:51:15 -0400

    Codecache project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codecache::Application.config.secret_token = '16f3a5ab0219ec4bff4bdef5593f75ebb5ce40d60e842395a679327c85766ac0ba5e5dcc15b4c7563e2fb79ac78139932cfa1533a8f214b81552f87b01bdc47a'
